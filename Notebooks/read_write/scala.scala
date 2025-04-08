0fd64d07bbe42c5834cbc381d0fa7be3367f8768
skarpecki <skarpecki>
2025-04-08 16:18:46 +0000

    Base notebooks for read write

---
+++ Notebooks/read_write/scala.scala
@@ -0,0 +1,29 @@
+// Databricks notebook source
+// MAGIC %run "../utils"
+
+// COMMAND ----------
+
+val start = System.currentTimeMillis
+
+val n = 7
+val table = "samples.tpch.orders"
+
+var df = spark.table(table)
+for (_ <- 1 to n) {
+  df = df.union(spark.table(table))
+}
+
+df.write
+  .mode("overwrite")
+  .saveAsTable("bronze.default.orders_scala")
+
+
+val end = System.currentTimeMillis
+
+// COMMAND ----------
+
+listener.stageMetrics.toDF.show()
+
+// COMMAND ----------
+
+println((end - start) / 1000)
