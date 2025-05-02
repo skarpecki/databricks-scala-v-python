5ec842c2edf2ee7688f4bcc05718480c66c626fa
skarpecki <szymon.karpecki@gmail.com>
2025-05-02 17:32:38 +0200

    Added RW test class

---
+++ Modules/Python/src/databricks_libs/tests.py
@@ -0,0 +1,31 @@
+class Test:
+    def test_func():
+        raise NotImplementedError
+
+
+class ReadWriteTest(Test):
+    def __init__(self, spark):
+        self.spark = spark
+    def test_func(self):
+        n = 7
+        table = "samples.tpch.orders"
+
+        df = self.spark.read.table(table)
+        for _ in range(n):
+            df = df.union(
+                self.spark.read.table(table)
+            )
+
+        (
+            df.write
+            .mode("overwrite")
+            .saveAsTable("bronze.default.orders")
+        )
+
+        return df.explain()
+
+class TestsFactory:
+    @staticmethod
+    def get_test_func(code: str, spark) -> Test:
+        if code == 'rw_test':
+            return ReadWriteTest(spark).test_func
