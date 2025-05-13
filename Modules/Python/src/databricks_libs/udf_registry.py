3b698d769258526f0ebecdff41a6bd9aa357578e
skarpecki <szymon.karpecki@gmail.com>
2025-05-13 12:02:13 +0200

    Fixed register

--- Modules/Python/src/databricks_libs/udf_registry.py
+++ Modules/Python/src/databricks_libs/udf_registry.py
@@ -7,15 +7,17 @@ class UdfRegistry:
     LEFT_PANDAS_UDF = "left_pandas_udf"
     LEFT_SCALA_UDF = "left_scala_udf"
 
-    def _left_udf(self, str_val, len):
-        return str_val[:len]
+    def _left_udf_factory(self):
+        def _left_udf(str_val, len):
+            return str_val[:len]
+        return F.udf(_left_udf, StringType())
     
     def _left_pandas_udf_factory(self, n):
         def left_pandas_udf(s: pd.Series) -> pd.Series:
-            return s.str.slice(0, 3) 
+            return s.str.slice(0, n) 
         return F.pandas_udf(left_pandas_udf, StringType())
     
     def register_udf(self, spark):
         # Left python udf
-        spark.udf.register(UdfRegistry.LEFT_PYTHON_UDF, F.udf(self._left_udf, StringType()))
-        spark.udf.register(UdfRegistry.LEFT_PANDAS_UDF, F.udf(self._left_pandas_udf_factory(3), StringType()))
+        spark.udf.register(UdfRegistry.LEFT_PYTHON_UDF, self._left_udf_factory())
+        spark.udf.register(UdfRegistry.LEFT_PANDAS_UDF, self._left_pandas_udf_factory(3))
