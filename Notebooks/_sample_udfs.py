560901b74bb0f4a13a4242ab0b3fdc7a0b52fed2
skarpecki <skarpecki>
2025-05-25 15:29:51 +0000

    Added pandas UDF

--- Notebooks/_sample_udfs.py
+++ Notebooks/_sample_udfs.py
@@ -35,14 +35,32 @@ spark.udf.register("left_python_udf", left_udf)
 
 # COMMAND ----------
 
+# Necessary imports
+from pyspark.sql.functions import pandas_udf
+from pyspark.sql.types import StringType
+import pandas as pd
+
+# Define pandas function
+def left_3_pandas(s: pd.Series) -> pd.Series:
+    return s.str.slice(0, 3) 
+
+# Make it pandas_udf
+left_pandas_udf = pandas_udf(left_3_pandas, StringType())
+
+# Register UDF
+spark.udf.register("left_pandas_udf", left_pandas_udf)
+
+# COMMAND ----------
+
 # Prepare sample DataFramea
-data = [("example1", "example1"), ("example2", "example1"), ("example3", "example1")]
-columns = ["col1", "col2"]
+data = [("example1", "example2", "example3")]
+columns = ["col1"]
 df = spark.createDataFrame(data, columns)
 
 # Display result
 display(
     df.selectExpr(
-        "left_scala_udf(col1, 3) as col1_left",
-        "left_python_udf(col2, 3) as col2_left")
+        "left_scala_udf(col1, 3) as col1_scala",
+        "left_python_udf(col1, 3) as col1_python",
+        "left_python_udf(col1, 3) as col1_pandas",)
 )
